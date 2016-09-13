d5f7a1ecd086b54a4a2a71fd6467bb03eedecd1b
Alexander Shmyga <shmyga.87@gmail.com>
2016-09-13 11:42:25 +0200

    init commit

---
+++ Demo/Contact.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Demo
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Company { get; set; }
+        public string Title { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} - {1} {2} ({3}) - {4} ({5})",
+                this.Id, this.FirstName, this.LastName, this.Title, this.Company, this.Email);
+        }
+    }
+}
---
+++ Demo/Program.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Dapper DEMO";
+
+            var connectionString = 
+                ConfigurationManager.ConnectionStrings["contactsDB"].ConnectionString;
+
+            IDbConnection db = new SqlConnection(connectionString);
+            var contacts = 
+                db.Query<Contact>("select * from Contacts where Id = @Id", new { Id = 1 });
+
+            foreach (Contact contact in contacts)
+            {
+                Console.WriteLine(contact.ToString());
+            }
+
+            Console.WriteLine("Done.");
+            Console.ReadKey();
+        }
+    }
+}
